040b468d08322a66361d231884a2426dd7ec012c
hanyeong0815 <yoehadu815@gmail.com>
2023-03-30 09:44:44 +0900

    test: redis test

---
+++ src/main/java/com/self/music/domain/RefreshTokenRedis.java
@@ -0,0 +1,37 @@
+package com.self.music.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.time.Instant;
+
+@Getter
+@RedisHash(value = "refresh", timeToLive = 2628000L)
+public class RefreshTokenRedis {
+    @Id
+    private Long id;
+
+    private String token;
+
+    private String subject;
+
+    private Boolean enabled;
+
+    private Instant createdAt;
+
+    private Instant expiredAt;
+
+    private String replacedBy;
+
+    public RefreshTokenRedis(String subject, String token, Instant now, Long expiry){
+        this.subject = subject;
+        this.token = token;
+        enabled = true;
+        createdAt = now;
+        expiredAt = now.plusSeconds(expiry);
+    }
+}
---
+++ src/main/java/com/self/music/domain/RefreshTokenRedisRepo.java
@@ -0,0 +1,13 @@
+package com.self.music.domain;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface RefreshTokenRedisRepo extends CrudRepository<RefreshTokenRedis, String> {
+    @Transactional
+    @Modifying
+    @Query("update RefreshToken r set r.enabled = ?1 where r.subject = ?2")
+    void updateEnabledBySubject(boolean b, String subject);
+}
